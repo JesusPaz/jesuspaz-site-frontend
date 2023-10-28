5fbceb32b7efe10e697fbf393a977a9925a08f12
Jesus Hernando Paz Bolaños <jesus@nodesource.com>
2023-10-27 21:07:43 -0500

    it works

--- src/components/CompoundInterestCalculator.js
+++ src/components/CompoundInterestCalculator.js
@@ -11,8 +11,57 @@ export default function CompoundInterestCalculator() {
     const [contribution, setContribution] = useState(0);
     const [contributionFrequency, setContributionFrequency] = useState(1);
     const [result, setResult] = useState([]);
+    const [errors, setErrors] = useState({});
+    const resetValues = () => {
+        setPrincipal(0);
+        setRate(0);
+        setTime(0);
+        setFrequency(1);
+        setContribution(0);
+        setContributionFrequency(1);
+        setResult([]);
+    };
+    const validateFields = () => {
+        let isValid = true;
+        let newErrors = {};
 
+        if (principal <= 0 || !Number.isInteger(principal)) {
+            isValid = false;
+            newErrors.principal = "⚠️ El capital inicial debe ser un número entero mayor a 0.";
+        }
+        if (principal > 100000000) {
+            isValid = false;
+            newErrors.principal = "⚠️ El capital inicial debe ser menor a 100000000.";
+        }
+        if (rate <= 0) {
+            isValid = false;
+            newErrors.rate = "⚠️ La tasa de interés debe ser mayor a 0.";
+        }
+        if (rate > 100) {
+            isValid = false;
+            newErrors.rate = "⚠️ La tasa de interés debe ser menor a 100.";
+        }
+        if (!Number.isInteger(time) || time <= 0) {
+            isValid = false;
+            newErrors.time = "⚠️ La cantidad de años debe ser un número entero y mayor a 0.";
+        }
+        if (time > 200) {
+            isValid = false;
+            newErrors.time = "⚠️ La cantidad de años debe ser menor a 200.";
+        }
+
+        // Validar que la tasa tenga un máximo de 5 decimales
+        const decimalPlaces = (rate.toString().split('.')[1] || []).length;
+        if (decimalPlaces > 5) {
+            isValid = false;
+            newErrors.rate = "⚠️ La tasa de interés puede tener hasta 5 decimales.";
+        }
+
+        setErrors(newErrors);
+        return isValid;
+    };
     const calculateInterest = async () => {
+        if (!validateFields()) return;
         const response = await fetch(`${process.env.API_URL}/finance/compound-interest`, {
             method: 'POST',
             headers: {
@@ -48,11 +97,16 @@ export default function CompoundInterestCalculator() {
                         onChange={(e) => setPrincipal(e.target.value)}
                         variant="outlined"
                         margin="normal"
-                        className={styles.formControl}
+                        className={`${styles.fullText} ${errors.principal ? styles.errorField : ''}`}
+                        helperText={errors.principal}
+                        error={!!errors.principal}
+                    // FormHelperTextProps={{
+                    //     className: styles.errorText
+                    // }}
                     />
                 </div>
             </div>
-            {/* Paso 4: Interés */}
+            {/* Paso 2: Interés */}
             <div className={styles.step}>
                 <div className={styles.step}>
                     <h3>Interés</h3>
@@ -68,6 +122,8 @@ export default function CompoundInterestCalculator() {
                             margin="normal"
                             fullWidth
                             className={styles.formControl}
+                            helperText={errors.rate}
+                            error={!!errors.rate}
                         />
                         <FormControl variant="outlined" className={styles.formControl} fullWidth>
                             <InputLabel id="frequency-label">Frecuencia de capitalización</InputLabel>
@@ -78,10 +134,10 @@ export default function CompoundInterestCalculator() {
                                 onChange={(e) => setFrequency(e.target.value)}
                                 label="Frecuencia de capitalización"
                             >
-                                <MenuItem value={1}>Anual</MenuItem>
-                                <MenuItem value={2}>Semestral</MenuItem>
-                                <MenuItem value={4}>Trimestral</MenuItem>
-                                <MenuItem value={12}>Mensual</MenuItem>
+                                <MenuItem value={1}>cada año</MenuItem>
+                                <MenuItem value={2}>dos veces al año</MenuItem>
+                                <MenuItem value={4}>cada tres meses</MenuItem>
+                                <MenuItem value={12}>cada mes</MenuItem>
                             </Select>
                         </FormControl>
                     </div>
@@ -101,7 +157,9 @@ export default function CompoundInterestCalculator() {
                         onChange={(e) => setTime(e.target.value)}
                         variant="outlined"
                         margin="normal"
-                        className={styles.formControl}
+                        className={styles.fullText}
+                        helperText={errors.time}
+                        error={!!errors.time}
                     />
                 </div>
             </div>
@@ -131,20 +189,24 @@ export default function CompoundInterestCalculator() {
                                 onChange={(e) => setContributionFrequency(e.target.value)}
                                 label="Frecuencia de los aportes"
                             >
-                                <MenuItem value={1}>Anual</MenuItem>
-                                <MenuItem value={2}>Semestral</MenuItem>
-                                <MenuItem value={4}>Trimestral</MenuItem>
-                                <MenuItem value={12}>Mensual</MenuItem>
+                                <MenuItem value={1}>cada año</MenuItem>
+                                <MenuItem value={2}>dos veces al año</MenuItem>
+                                <MenuItem value={4}>cada tres meses</MenuItem>
+                                <MenuItem value={12}>cada mes</MenuItem>
                             </Select>
                         </FormControl>
                     </div>
                 </div>
             </div>
 
-
-            <Button variant="contained" color="primary" onClick={calculateInterest}>
-                Calcular
-            </Button>
+            <div className={styles.buttonContainer}>
+                <Button variant="outlined" color="secondary" className={styles.whiteButton} onClick={() => resetValues()}>
+                    Reiniciar
+                </Button>
+                <Button variant="contained" color="primary" className={styles.blueButton} onClick={calculateInterest}>
+                    Calcular
+                </Button>
+            </div>
             {result.length > 0 && (
                 <div className={styles.result}>
                     <p>
