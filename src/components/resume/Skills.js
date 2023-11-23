97334a969dcb72f9c9e6c88e315f66e9d59f1ace
Jesus Hernando Paz Bolaños <jesus@nodesource.com>
2023-11-23 17:09:58 -0500

    imrpove skills

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -1,32 +1,55 @@
 import React from 'react';
-import { Box, Typography, Grid } from '@mui/material';
-import CloudIcon from '@mui/icons-material/Cloud'; // Ejemplo de icono para plataformas en la nube
-import DockerIcon from '@mui/icons-material/Dock'; // Ejemplo de icono para contenedores
-import CodeIcon from '@mui/icons-material/Code'; // Ejemplo de icono para IaC, CI/CD, etc.
-import StorageIcon from '@mui/icons-material/Storage'; // Ejemplo de icono para monitoreo y logging
-import SettingsIcon from '@mui/icons-material/Settings'; // Ejemplo de icono para configuración
+import { Box, Grid, Tooltip, Zoom, Typography } from '@mui/material';
+import CloudIcon from '@mui/icons-material/Cloud';
+import DockerIcon from '@mui/icons-material/Dock';
+import CodeIcon from '@mui/icons-material/Code';
+import StorageIcon from '@mui/icons-material/Storage';
+import SettingsIcon from '@mui/icons-material/Settings';
+import styles from '../../styles/SkillStyles.module.css';
 
-const SkillCategory = ({ title, children }) => {
-    return (
-        <Box marginBottom={4}>
-            <Typography variant="h6" style={{ fontWeight: 'bold', marginBottom: '10px' }}>{title}</Typography>
-            <Grid container spacing={2}>
-                {children}
-            </Grid>
-        </Box>
-    );
+
+const skillStyle = {
+    cursor: 'pointer',
+    transition: 'transform 0.3s ease',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '120px', // Aumentado el tamaño del Box
+    width: '120px',
+    borderRadius: '50%',
+    margin: 'auto',
 };
 
 const SkillItem = ({ icon, title }) => {
     return (
-        <Grid item xs={12} sm={6} md={4}>
-            <Box display="flex" flexDirection="column" alignItems="center">
-                {icon}
-                <Typography variant="h6" style={{ marginTop: '10px' }}>{title}</Typography>
-            </Box>
+        <Grid item xs={4} sm={3} md={2} lg={1}>
+            <Tooltip
+                title={title}
+                placement="bottom"
+                TransitionComponent={Zoom}
+                classes={{
+                    tooltip: styles.skillTooltip,
+                    arrow: styles.skillTooltipArrow
+                }}
+                arrow
+            >
+                <Box
+                    className="skill-icon"
+                    sx={{
+                        ...skillStyle,
+                        // marginBottom: '-10px'
+                    }}
+                    onMouseOver={(e) => e.currentTarget.style.transform = 'scale(1.8)'}
+                    onMouseOut={(e) => e.currentTarget.style.transform = 'scale(1)'}
+                >
+                    {React.cloneElement(icon, { fontSize: 'large' })}
+                </Box>
+            </Tooltip>
         </Grid>
     );
 };
+
+
 
 const Skills = () => {
     return (
@@ -34,39 +57,12 @@ const Skills = () => {
             <Typography variant="h4" style={{ fontWeight: 'bold', marginBottom: '20px', textAlign: 'center' }}>
                 Skills
             </Typography>
-            <SkillCategory title="Cloud Platforms">
+            <Box display="flex" justifyContent="center" flexWrap="wrap">
                 <SkillItem icon={<CloudIcon />} title="AWS" />
                 <SkillItem icon={<CloudIcon />} title="Azure" />
-            </SkillCategory>
-            <SkillCategory title="Containerization">
                 <SkillItem icon={<DockerIcon />} title="Docker" />
-                <SkillItem icon={<DockerIcon />} title="Kubernetes" />
-                <SkillItem icon={<DockerIcon />} title="Helm" />
-            </SkillCategory>
-            <SkillCategory title="Infrastructure as Code (IaC)">
-                <SkillItem icon={<CodeIcon />} title="Terraform" />
-                <SkillItem icon={<CodeIcon />} title="CloudFormation" />
-            </SkillCategory>
-
-            <SkillCategory title="Monitoring & Logging">
-                <SkillItem icon={<StorageIcon />} title="Grafana" />
-                <SkillItem icon={<StorageIcon />} title="Prometheus" />
-                <SkillItem icon={<StorageIcon />} title="ELK Stack" />
-            </SkillCategory>
-            <SkillCategory title="Scripting">
-                <SkillItem icon={<StorageIcon />} title="Python" />
-                <SkillItem icon={<StorageIcon />} title="Bash" />
-                <SkillItem icon={<StorageIcon />} title="PowerShell" />
-            </SkillCategory>
-            <SkillCategory title="Configuration Management">
-                <SkillItem icon={<SettingsIcon />} title="Ansible" />
-                <SkillItem icon={<SettingsIcon />} title="Chef" />
-            </SkillCategory>
-            <SkillCategory title="CI/CD">
-                <SkillItem icon={<CodeIcon />} title="Jenkins" />
-                <SkillItem icon={<CodeIcon />} title="GitLab CI" />
-                <SkillItem icon={<CodeIcon />} title="GitHub Actions" />
-            </SkillCategory>
+                {/* ... otros íconos de habilidades ... */}
+            </Box>
         </Box>
     );
 };
